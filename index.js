05708963fb1c7c8a4eb3f95cb62e9a2b1d4d9fc2
nick bryant <nbryant@joshuaschool.org>
2014-09-23 01:46:52 +0000

    deploying again

---
+++ index.js
@@ -0,0 +1,15 @@
+var texturePath = require('programmerart-textures')('');
+var createGame = require('voxel-engine');
+var container = document.body;
+
+    var game = createGame({
+        texturePath: texturePath,
+        materials: [
+            ['blocks/grass_top', 'blocks/dirt', 'blocks/grass_side'],
+            'blocks/stone',
+            'blocks/dirt']
+        });
+game.appendTo(container);
+var createPlayer = require('voxel-player');
+var test = createplayer()(game);
+test.possess();
