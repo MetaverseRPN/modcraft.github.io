42aebe4d9980e40ccb0f7ab2f4f4c306463778f5
nick bryant <nbryant@joshuaschool.org>
2014-09-24 17:58:35 +0000

    Regenerate bundle.js using deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -x
-browserify index.coffee -t coffeeify -o bundle.js
+browserify index.coffee -t coffeeify -o bundle1.js
 git add -A
 git commit -am "Regenerate bundle.js using deploy.sh"
 git push origin gh-pages
